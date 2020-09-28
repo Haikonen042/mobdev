7c2f1e0d5cfb5b527a34dcb48aacbdfba6a914b3
krista <krisby@coursera-smt.com>
2020-09-28 14:53:33 +0300

    update

--- find_address/App.js
+++ find_address/App.js
@@ -4,27 +4,26 @@ import MapView, { Marker } from "react-native-maps";
 
 export default function App() {
   const [address, setAddress] = useState("");
-  const [coordinates, setCoordinates] = useState([]);
+  const [coordinates, setCoordinates] = useState({});
   const [lat, setLat] = useState(60.200692);
   const [long, setLong] = useState(24.934302);
 
-  const getCoordinates = () => {
+  const getCoordinates = async () => {
     const url =
       "http://www.mapquestapi.com/geocoding/v1/address?key=4zM1h33gHvrwbixoSkxhBlANTBr0fxRj&location=" +
       address +
       "+finland";
-    fetch(url)
-      .then((response) => response.json())
-      .then((responseJson) => {
-        setCoordinates(responseJson.results.locations.latLng);
-        Alert(coordinates);
-      })
-      .catch((error) => {
-        Alert.alert("Error", error);
-      });
 
-    /*setLat(coordinates.lat);
-    setLong(coordinates.lng);*/
+    try {
+      const response = await fetch(url);
+      const coordinateData = await response.json();
+      setCoordinates(coordinateData.results[0].locations[0].displayLatLng);
+    } catch (e) {
+      Alert.alert("Error fetching data");
+    }
+
+    setLat(coordinates.lat);
+    setLong(coordinates.lng);
   };
 
   return (
@@ -32,16 +31,13 @@ export default function App() {
       <MapView
         style={{ flex: 1 }}
         region={{
-          latitude: 60.200692,
-          longitude: 24.934302,
+          latitude: lat,
+          longitude: long,
           latitudeDelta: 0.0322,
           longitudeDelta: 0.0221,
         }}
       >
-        <Marker
-          coordinate={{ latitude: lat, longitude: long }}
-          title="Result"
-        />
+        <Marker coordinate={{ latitude: lat, longitude: long }} />
       </MapView>
       <TextInput
         style={styles.input}
